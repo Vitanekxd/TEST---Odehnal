0c334aef9c82ec90db8a49521b2dfb9a8c1c69ca
Vít Odehnal <odehnal11@meail.cz>
2022-05-17 11:38:22 +0200

    Přidejte soubory projektu.

---
+++ TEST - Odehnal/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TEST___Odehnal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        nakladak tatra;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tatra = new nakladak();
+            txt1.Text = tatra.ToString();
+            Shownakladak(tatra, txt1);
+        }
+        public void Shownakladak(nakladak nakladak, TextBox textbox )
+        {
+            textbox.Text = $"Maximalni Nosnost: {nakladak.MaxNosnost}¨\n";
+            textbox.Text += $"Naklad: {nakladak.Naklad }\n";
+            textbox.Text += $"Velikost Nadrze: {nakladak.VelikostNadrze}\n";
+            textbox.Text += $"Stav Paliva: {nakladak.StavPaliva}\n";
+            textbox.Text += $"Stav Tachometr: {nakladak.StavTachometr}\n";
+            textbox.Text += $"Spotreba Paliva: {nakladak.SpotrebaPaliva}\n";
+
+        }
+
+    }
+}
---
+++ TEST - Odehnal/nakladak.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST___Odehnal
+{
+    internal class nakladak
+    {
+        public int Jmeno { get; set; }
+        public int MaximalniNosnost { get; set; }
+        public int VelikostNadrze { get; }
+        public int SpotrebaPaliva { get; }
+
+        public int StavPaliva { get; set; }
+        public int StavTachometr { get; set; }
+        public int Naklad { get; }
+        public nakladak()
+
+        {
+            Jmeno = 
+            MaxNosnost = 400;
+            VelikostNadrze = 150;
+            SpotrebaPaliva = 40;
+            Naklad = 0;
+        }
+        public void vylozit()
+        {
+            Naklad -= 400;
+            if (Naklad < 0)
+            {
+                Naklad = 0;
+            }
+
+        }
+        public void Nalozit()
+        {
+            Naklad += 400;
+            if (Naklad < 0)
+            {
+                Naklad = 0;
+            }
+
+        }
+
+    }
+    
+}
